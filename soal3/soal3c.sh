09d9fdbbeda6a0b4b4e00759778d8800f7e4e5e7
Hanifa Fauziah <80946219+hanifaafauziah@users.noreply.github.com>
2021-03-27 19:38:14 +0700

    soal no 3

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -1,45 +1,12 @@
 #!/bin/bash
 
 tanggal=$(date +"%d-%m-%Y")
-bantu=$(date +"%d")
+kmrn=$(date --date="yesterday" +"%d-%m-%Y")
 
-x=$(($bantu%2))
-
-if(($x==0))
+#kalau baru mulai
+#kalau kemaren cetak kucing
+if [[ -d "/home/hanifa/Desktop/praktikum1/soal3/Kucing_$kmrn" || ! -d "/home/hanifa/Desktop/praktikum1/soal3/Kucing_$kmrn" && ! -d "/home/hanifa/Desktop/praktikum1/soal3/Kelinci_$kmrn" ]]
 then
-    mkdir "Kucing_$tanggal"
-    cd Kucing_$tanggal
-    for (( i=1; i<=23; i++))
-    do
-      wget -O "Koleksi_$i.jpeg" -a Foto.log https://loremflickr.com/320/240/kitten 
-    done
-    for (( a=1; a<=23; a++ ))
-    do
-       for(( b=a+1; b<=23; b++ ))
-       do
-          cmp -s "Koleksi_$a.jpeg" "Koleksi_$b.jpeg"
-          if [ $? == 0 ]
-          then
-               rm "Koleksi_$b.jpeg"
-          fi 
-       done
-    done
-
-    num=1
-    for f in *.jpeg
-    do
-        if((num<10))
-        then
-             mv -- "$f" "Koleksi_0$num"
-             num=$((num+1))
-        else
-             mv -- "$f" "Koleksi_$num"
-             num=$((num+1))
-        fi
-
-    done
-
-else
     mkdir "Kelinci_$tanggal"
     cd Kelinci_$tanggal
     for (( i=1; i<=23; i++))
@@ -71,9 +38,45 @@ else
         fi
 
     done
-
+    cd ..
 fi
 
+#kalau kemaren cetak kelinci
+if [ -d "/home/hanifa/Desktop/praktikum1/soal3/Kelinci_$kmrn" ]
+then 
+    mkdir "Kucing_$tanggal"
+    cd Kucing_$tanggal
+    for (( i=1; i<=23; i++))
+    do
+      wget -O "Koleksi_$i.jpeg" -a Foto.log https://loremflickr.com/320/240/kitten 
+    done
+    for (( a=1; a<=23; a++ ))
+    do
+       for(( b=a+1; b<=23; b++ ))
+       do
+          cmp -s "Koleksi_$a.jpeg" "Koleksi_$b.jpeg"
+          if [ $? == 0 ]
+          then
+               rm "Koleksi_$b.jpeg"
+          fi 
+       done
+    done
+
+    num=1
+    for f in *.jpeg
+    do
+        if((num<10))
+        then
+             mv -- "$f" "Koleksi_0$num"
+             num=$((num+1))
+        else
+             mv -- "$f" "Koleksi_$num"
+             num=$((num+1))
+        fi
+
+    done
+    cd ..
+fi
 
 
 
